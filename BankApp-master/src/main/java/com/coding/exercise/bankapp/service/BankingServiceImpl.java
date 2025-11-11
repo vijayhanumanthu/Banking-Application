2cb6ec003dd0e6aa6f6d559a155087afc67f661d
vijayhanumanthu <119610340+vijayhanumanthu@users.noreply.github.com>
2025-11-11 17:15:17 -0600

    adding implementation for transfering Details

--- BankApp-master/src/main/java/com/coding/exercise/bankapp/service/BankingServiceImpl.java
+++ BankApp-master/src/main/java/com/coding/exercise/bankapp/service/BankingServiceImpl.java
@@ -67,7 +67,7 @@ public class BankingServiceImpl implements BankingService {
 		
 	}
 
-		public ResponseEntity<Object> addNewAccount(AccountInformation accountInformation, Long customerNumber) {
+	public ResponseEntity<Object> addNewAccount(AccountInformation accountInformation, Long customerNumber) {
 		
 		Optional<Customer> customerEntityOpt = customerRepository.findByCustomerNumber(customerNumber);
 
@@ -85,5 +85,74 @@ public class BankingServiceImpl implements BankingService {
 		return ResponseEntity.status(HttpStatus.CREATED).body("New Account created successfully.");
 	}
 
+	
+	public ResponseEntity<Object> transferDetails(TransferDetails transferDetails, Long customerNumber) {
+		
+		List<Account> accountEntities = new ArrayList<>();
+		Account fromAccountEntity = null;
+		Account toAccountEntity = null;
+		
+		Optional<Customer> customerEntityOpt = customerRepository.findByCustomerNumber(customerNumber);
+
+		// If customer is present
+		if(customerEntityOpt.isPresent()) {
+			
+			// get FROM ACCOUNT info
+			Optional<Account> fromAccountEntityOpt = accountRepository.findByAccountNumber(transferDetails.getFromAccountNumber());
+			if(fromAccountEntityOpt.isPresent()) {
+				fromAccountEntity = fromAccountEntityOpt.get();
+			}
+			else {
+			// if from request does not exist, 404 Bad Request
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("From Account Number " + transferDetails.getFromAccountNumber() + " not found.");
+			}
+			
+			
+			// get TO ACCOUNT info
+			Optional<Account> toAccountEntityOpt = accountRepository.findByAccountNumber(transferDetails.getToAccountNumber());
+			if(toAccountEntityOpt.isPresent()) {
+				toAccountEntity = toAccountEntityOpt.get();
+			}
+			else {
+			// if from request does not exist, 404 Bad Request
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("To Account Number " + transferDetails.getToAccountNumber() + " not found.");
+			}
+
+			
+			// if not sufficient funds, return 400 Bad Request
+			if(fromAccountEntity.getAccountBalance() < transferDetails.getTransferAmount()) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Insufficient Funds.");
+			}
+			else {
+				synchronized (this) {
+					// update FROM ACCOUNT 
+					fromAccountEntity.setAccountBalance(fromAccountEntity.getAccountBalance() - transferDetails.getTransferAmount());
+					fromAccountEntity.setUpdateDateTime(new Date());
+					accountEntities.add(fromAccountEntity);
+					
+					// update TO ACCOUNT
+					toAccountEntity.setAccountBalance(toAccountEntity.getAccountBalance() + transferDetails.getTransferAmount());
+					toAccountEntity.setUpdateDateTime(new Date());
+					accountEntities.add(toAccountEntity);
+					
+					accountRepository.saveAll(accountEntities);
+					
+					// Create transaction for FROM Account
+					Transaction fromTransaction = bankingServiceHelper.createTransaction(transferDetails, fromAccountEntity.getAccountNumber(), "DEBIT");
+					transactionRepository.save(fromTransaction);
+					
+					// Create transaction for TO Account
+					Transaction toTransaction = bankingServiceHelper.createTransaction(transferDetails, toAccountEntity.getAccountNumber(), "CREDIT");
+					transactionRepository.save(toTransaction);
+				}
+
+				return ResponseEntity.status(HttpStatus.OK).body("Success: Amount transferred for Customer Number " + customerNumber);
+			}
+				
+		} else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer Number " + customerNumber + " not found.");
+		}
+		
+	}
 
 }
