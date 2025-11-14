c4ad5e24c0c92609eeda3b8666ded5e7b19ba8a9
vijayhanumanthu <119610340+vijayhanumanthu@users.noreply.github.com>
2025-11-14 15:59:24 -0600

    Create CustomerController.java

---
+++ BankApp-master/src/main/java/com/coding/exercise/bankapp/controller/CustomerController.java
@@ -0,0 +1,41 @@
+package com.coding.exercise.bankapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coding.exercise.bankapp.domain.CustomerDetails;
+import com.coding.exercise.bankapp.service.BankingServiceImpl;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@RequestMapping("customers")
+@Api(tags = { "Customer REST endpoints" })
+public class CustomerController {
+
+	@Autowired
+	private BankingServiceImpl bankingService;
+
+	@GetMapping(path = "/all")
+	@ApiOperation(value = "Find all customers", notes = "Gets details of all the customers")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success"),
+			@ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 500, message = "Internal Server Error") })
+
+	public List<CustomerDetails> getAllCustomers() {
+
+		return bankingService.findAll();
+	}
