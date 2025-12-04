35a26518e99f6f5d7aee34588b8a5cda1384829b
vijayhanumanthu <119610340+vijayhanumanthu@users.noreply.github.com>
2025-12-04 13:45:51 -0600

    delete customer feature added

--- BankApp-master/src/main/java/com/coding/exercise/bankapp/controller/CustomerController.java
+++ BankApp-master/src/main/java/com/coding/exercise/bankapp/controller/CustomerController.java
@@ -62,3 +62,15 @@ public class CustomerController {
 
 		return bankingService.updateCustomer(customerDetails, customerNumber);
 	}
+	
+	@DeleteMapping(path = "/{customerNumber}")
+	@ApiOperation(value = "Delete customer and related accounts", notes = "Delete customer and all accounts associated with him.")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = Object.class),
+			@ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 500, message = "Internal Server Error") })
+
+	public ResponseEntity<Object> deleteCustomer(@PathVariable Long customerNumber) {
+
+		return bankingService.deleteCustomer(customerNumber);
+	}
+}
