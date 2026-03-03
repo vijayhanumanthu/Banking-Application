b510b77759a458ced80d00bff1cdd7b5dd87b994
vijayhanumanthu <vijayhanumanthums@gmail.com>
2026-03-03 16:08:12 -0600

    BankInformation Class updated

--- BankApp-master/src/main/java/com/coding/exercise/bankapp/domain/BankInformation.java
+++ BankApp-master/src/main/java/com/coding/exercise/bankapp/domain/BankInformation.java
@@ -13,6 +13,38 @@ import lombok.Setter;
 @Builder
 public class BankInformation {
 
+	public String getBranchName() {
+		return branchName;
+	}
+
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+
+	public Integer getBranchCode() {
+		return branchCode;
+	}
+
+	public void setBranchCode(Integer branchCode) {
+		this.branchCode = branchCode;
+	}
+
+	public AddressDetails getBranchAddress() {
+		return branchAddress;
+	}
+
+	public void setBranchAddress(AddressDetails branchAddress) {
+		this.branchAddress = branchAddress;
+	}
+
+	public Integer getRoutingNumber() {
+		return routingNumber;
+	}
+
+	public void setRoutingNumber(Integer routingNumber) {
+		this.routingNumber = routingNumber;
+	}
+
 	private String branchName;
 	
 	private Integer branchCode;
